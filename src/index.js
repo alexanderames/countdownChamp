79603ad6da19377bcd5a4992cad6d1d5507cdc00
alexander ames <alexanderthomasames@gmail.com>
2017-06-14 09:45:25 -0700

    create simple component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.jsx';
 
 ReactDOM.render(
-	<div>Coundown Champ</div>, document.getElementById('root')
+	<App></App>,
+	document.getElementById('root')
 );
